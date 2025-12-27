23761ec6572d9327fe5714df9afb5feb208471d2
Dachacho <arevadzedachi123@gmail.com>
2025-12-28 01:57:34 +0400

    add remove button

--- main.c
+++ main.c
@@ -9,7 +9,7 @@ typedef struct {
 
 Arr arr;
 
-int* add(int num){
+int* add_to_arr(int num){
 	int *tmp;
 	int new_size = 0;
 	if(arr.idx < arr.size){
@@ -32,6 +32,29 @@ int* add(int num){
 	return arr.ptr;
 }
 
+int* remove_from_arr(){
+	int *tmp;
+	int new_size = 0;
+
+	if(arr.idx * 2 < arr.size && arr.size > 8){
+		new_size = arr.size / 2;
+		tmp = realloc(arr.ptr, new_size * sizeof(int));
+		if (!tmp){
+			perror("realoc problem");
+			return NULL;
+		}
+		arr.ptr = tmp;
+		arr.ptr[arr.idx] = 0;
+		arr.size = new_size;
+		arr.idx--;
+	}else{
+		arr.ptr[arr.idx] = 0;
+		arr.idx--;
+	}
+
+	return arr.ptr;
+}
+
 int main(){
 	arr.size = 8;
 	arr.idx = 0;
@@ -48,18 +71,37 @@ int main(){
 		return 0;
 	}
 	
-	for (int k = 0; k < arr.size; k++){
+	for (int k = 0; k < arr.idx; k++){
 		printf("%d ", arr.ptr[k]);
 	}
 
-	for (int i = 0; i < 2000; i++){
-		arr.ptr = add(2);
+	printf("\n");
+	for (int i = 0; i < 100; i++){
+		arr.ptr = add_to_arr(2);
 		if(arr.ptr == NULL){
 			printf("add failed\n");
 			return 1;
 		}
-	}	
-	
+	}
+
+	printf("\n");
+	for (int k = 0; k < arr.idx; k++){
+		printf("%d ", arr.ptr[k]);
+	}
+
+	for (int i = 0; i < 75; i++){
+		arr.ptr = remove_from_arr();
+		if(arr.ptr == NULL){
+			printf("add failed\n");
+			return 1;
+		}
+	}
+
+	printf("\n");
+	for (int k = 0; k < arr.idx; k++){
+		printf("%d ", arr.ptr[k]);
+	}
+
 	free(arr.ptr);
 	printf("\n");
 	printf("size of array: %d \n", arr.size);
